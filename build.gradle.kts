685fdf46ec925412a86655d1c73a79a34934d57d
maxb2718 <6156085+maxb2718@users.noreply.github.com>
2023-05-26 03:02:40 +0200

    Clean up gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,12 @@ repositories {
 }
 
 dependencies {
-//	TODO clean up dependencies
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.mockito:mockito-core")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
